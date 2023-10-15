3bc7d45f0bac0ddb7a455bf62edfadb684065891
Vicsimus89 <fastpitching06801@msn.com>
2023-10-14 18:44:34 -0600

    Created the main method for calculator app to include quotient and modulus, and then a second method for calcuating the area of a users radius they put in.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace OperatorExercise
+﻿using System.Globalization;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            var sum = a + b;
+
+            var sub = a - b;
+
+            var multiply = a * b;
+            
+            Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your cirlce?");
+
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse( userInput );
+
+            Console.WriteLine(AreaOfCircle( radius ) );
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
+            
         }
     }
+
 }
